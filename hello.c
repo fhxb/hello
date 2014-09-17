edd7f6e321d4e82560313336cfa39a649da6de97
liyupeng666 <liyupeng_666@126.com>
2014-09-17 16:03:50 +0800

    commit hello.c

---
+++ hello.c
@@ -0,0 +1,24 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file hello.c
+ * @author liyupeng01(liyupeng01@baidu.com)
+ * @date 2014/09/17 16:01:03
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+#include <stdio.h>
+int main()
+{
+    printf("hello!\n");
+    return 0;
+}
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
